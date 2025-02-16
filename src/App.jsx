a922411c4fb2872b5cfe4fd2723f3b05e05a4d07
Ermias Desalegn <ermidessalegn@gmail.com>
2025-02-16 19:00:52 +0300

    add proxy to fetch APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import instance from "./api/axiosConfig";
 import Navbar from "./components/Navbar.jsx";
 import AccountProfile from "./components/ui/AccountProfile.jsx";
 import Search from "./components/ui/Search.jsx";
 import SideBar from "./components/ui/SideBar.jsx";
+
 export default function App() {
+  // sample fetch
+  useEffect(() => {
+    async function fetchDepartments() {
+      try {
+        const res = await instance.get("/departments");
+        console.log(res.data);
+      } catch (error) {
+        if (error.response) {
+          console.error("Error Response Data:", error.response.data);
+          console.error("Status Code:", error.response.status);
+        } else {
+          console.error("Error Message:", error.message);
+        }
+      }
+    }
+    fetchDepartments();
+  }, []);
   return (
     <div className="text-3xl font-bold  h-svh">
       <Navbar />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy requests starting with `/api` to the backend
+      "/api": {
+        target: "https://mkhr-backend.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/v1.0.0"),
+      },
+    },
+  },
+});
